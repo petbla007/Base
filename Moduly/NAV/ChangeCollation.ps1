a7b01114ed30810033f72a28c428cf1143fc87b5
Petr Blažek <petr.blazek@webcom.cz>
2017-06-30 10:54:30 +0200

    Update

---
+++ Moduly/NAV/ChangeCollation.ps1
@@ -0,0 +1,19 @@
+﻿# Run as administrator
+
+Set-ExecutionPolicy unrestricted -Force
+Import-Module 'C:\Program Files\Microsoft Dynamics NAV\80\Service\NavAdminTool.ps1'
+
+
+# Create NEW instance
+
+Export-NAVData -DatabaseName BP2015 -FilePath c:\temp\BP2015 -CompanyName Test-školení -DatabaseServer NBPETBLA\SQL2012 -IncludeGlobalData -IncludeApplication
+
+
+New-NAVDatabase -DatabaseName BP2015CZ -FilePath C:\temp\backup\BP2015CZ.bak -DatabaseServer NBPETBLA\SQL2012 -DestinationPath C:\MSSQL
+Import-NAVData -DatabaseName BP2015CZ -FilePath c:\temp\BP2015.navdata -CompanyName Test-školení -DatabaseServer NBPETBLA\SQL2012 -IncludeApplicationData -IncludeGlobalData
+
+
+# Testovací část 
+
+
+
--- Skrypty/BaseConfig.ps1
+++ Skrypty/BaseConfig.ps1
@@ -4,6 +4,7 @@ $DatabaseName   = "cust1"
 $ServerInstance = ""
 $NavInstance    = "DynamicsNAV100Cust"
 $WebInstance    = ""
+$portRTC        = "7046"
 
 # Custom Setting
 $CustVPN        = $true
