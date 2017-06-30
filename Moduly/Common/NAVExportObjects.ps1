6bfa547918e51d9dbc4441d37be095abc40f1305
Petr Blažek <petr.blazek@webcom.cz>
2017-06-30 12:46:13 +0200

    Update

---
+++ Moduly/Common/NAVExportObjects.ps1
@@ -0,0 +1,20 @@
+﻿# Mudeles Import
+Import-Module 'C:\Program Files (x86)\Microsoft Dynamics NAV\80\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1'
+
+# Param
+$ServerInstance = "DynamicsNAV80_DEMO"
+$DatabaseName   = "Demo Database NAV (8-0-40938)"
+$DatabaseServer = ".\MSSQL2014" 
+$NAVObjectFile  = "NAVObjects80-40938.txt"
+$FilterString   = ""
+
+# Preparation 
+$FilePath       = "C:\temp\NAVApp\"
+
+Set-NAVServerConfiguration $ServerInstance -KeyName DatabaseName -KeyValue $DatabaseName
+Set-NAVServerInstance $ServerInstance -restart
+
+# Export Objects
+Export-NAVApplicationObject -DatabaseName $DatabaseName -DatabaseServer $DatabaseServer -Path "$FilePath$NAVObjectFile" -Filter $FilterString
+
+
---
+++ Moduly/NAV/NAVStartClient.ps1
@@ -0,0 +1,16 @@
+﻿# Mudeles Import
+Set-ExecutionPolicy unrestricted -Force
+Import-Module 'C:\Program Files (x86)\Microsoft Dynamics NAV\80\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1'
+Import-Module 'C:\Program Files\Microsoft Dynamics NAV\80\Service\NavAdminTool.ps1'
+Import-Module 'C:\Projekty\Tools\PowerShell\Scripts\NavCommonFunction.ps1'
+
+
+# Parameters
+$ServerName = "zlpetbla" 
+$ServerInstance = "DynamicsNAV80_DEMO"
+$ServerPort = 7046
+
+
+# Start RTC Client
+Start-NAVWindowsClient -ServerName $ServerName -ServerInstance $ServerInstance -Port $ServerPort
+
